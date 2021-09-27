dca06e804c4dc7aa1b6309eb219c3eb71cb86b33
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-09-28 00:39:51 +0300

    asdf

--- public/src/js/requests.js
+++ public/src/js/requests.js
@@ -41,4 +41,91 @@ export default class Request {
         })
 }
 
+  static cookieCheck() {
+    Ajax.promisifyGet({ url: `${backendAddress}/profile`, })
+        .then(({ status, responseText }) => {
+            // const homeView = new HomeView(root);
+            // homeView.render();
+            console.log(responseText);
+            // user = responseText;
+            // window.user = user;
+            return responseText;
+
+        })
+        .catch(({ status, responseText }) => {
+            // const authorizView = new AuthorizView(root);
+            // authorizView.render();
+            console.log('fail');
+        })
+}
+
+  static logIn({ body, alertObject }) {
+
+    Ajax.promisifyPost({ url: `${backendAddress}/login`, body })
+        .then(({ status, responseText }) => {
+
+            const root = document.getElementById("main-container");
+            const homeModel = new HomeModel(root);
+            homeModel.render();
+            // homePageRequest();
+
+            return;
+        })
+        .catch(({ status, responseText }) => {
+            alertObject.innerText = 'Неверное имя пользователя или пароль';
+            alertObject.style.visibility = 'visible';
+            console.log('error');
+        })
+}
+
+
+  static signUp({ body, alertObject }) {
+
+    Ajax.promisifyPost({ url: `${backendAddress}/signup`, body })
+        .then(({ status, responseText }) => {
+
+            const root = document.getElementById("main-container");
+            const homeModel = new HomeModel(root);
+            homeModel.render();
+            // homePageRequest();
+
+
+            console.log('success');
+            return;
+        })
+        .catch(({ status, responseText }) => {
+            alertObject.innerText = 'Пользователь уже существует';
+            alertObject.style.visibility = 'visible';
+        })
+}
+
+static logOut() {
+    Ajax.promisifyGet({ url: `${backendAddress}/logout`, })
+        .then(({ status, responseText }) => {
+            const root = document.getElementById("main-container");
+            const homeModel = new HomeModel(root);
+            homeModel.render();
+            // homePageRequest();
+        })
+        .catch(({ status, responseText }) => {
+            alert(responseText);
+        })
+}
+
+  static homePage() {
+    Ajax.promisifyGet({ url: `${backendAddress}/homepage`, })
+        .then(({ status, responseText }) => {
+
+            // console.log(responseText);
+
+            const root = document.getElementById("main-container");
+            const homeModel = new HomeModel(root);
+            // homeModel.Catalog = responseText;
+            homeModel.render();
+        })
+        .catch(({ status, responseText }) => {
+            alert(responseText);
+        })
+}
+
 }
