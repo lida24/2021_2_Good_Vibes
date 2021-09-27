9fa095ca688add48d44a0adb53036c018019fddf
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-09-28 01:13:08 +0300

    asdf

--- public/src/js/requests.js
+++ public/src/js/requests.js
@@ -2,7 +2,7 @@ const backendAddress = 'https://ozonback.herokuapp.com';
 
 import SigninModel from "../models/SigninModel.js";
 import HomeModel from "../models/HomeModel.js";
-import Ajax from "./ajax.js";
+// import Ajax from "./ajax.js";
 
 export default class Request {
 
