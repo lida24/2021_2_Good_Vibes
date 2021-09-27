714bf11ade8d2a6f99eeee0a838fc9fa54955b3c
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-09-28 01:43:40 +0300

    asdf

--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,3 @@
-import HomeModel from './models/HomeModel.js';
 import Hood from './models/Hood.js';
 import Request from './js/requests.js';
 
@@ -6,50 +5,7 @@ const hoodContainer = document.getElementsByClassName("grid-container")[0];
 const hood = new Hood(hoodContainer);
 hood.render();
 
-const root = document.getElementById('main-container');
-
-// const homeModel = new HomeModel(root);
-// homeModel.render();
-
 Request.homePage();
 
-// Request.homePageRequest();
-
-
-// let user;
-// coockieCheckRequest(user);
-
-// async function f(user) {
-//     return coockieCheckRequest(user);
-// }
-
-// let user = await f;
-
-// let result = cookieCheckRequest();
-
-// async function f() {
-
-//     let promise = new Promise((resolve, reject) => {
-//         resolve(coockieCheckRequest())
-//     });
-
-//     let result = await promise; 
-
-// }
-
-// f();
-
-// let response;
-// (async () => {
-//     let response = await cookieCheckRequest();
-//     // let user = await response.json();
-//     // let user = await console.log(response);
-//     console.log(response);
-// })();
-
-
-
-
-// console.log(user);
 
 
