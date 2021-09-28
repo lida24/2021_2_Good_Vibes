3301e21e3f838b748e1ab75ed6c032adec17f5f4
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-09-28 15:56:27 +0300

    asdf

--- public/src/models/RegisterModel.js
+++ public/src/models/RegisterModel.js
@@ -164,35 +164,35 @@ export default class RegisterModel {
       signinModel.render();
     });
 
-    this.#parent.addEventListener('submit', (e) => {
-      alert.style.visibility = 'hidden';
+    // this.#parent.addEventListener('submit', (e) => {
+    //   alert.style.visibility = 'hidden';
 
-      e.preventDefault();
+    //   e.preventDefault();
 
-      const usernameSignUpInput = document.getElementsByName('login')[0];
-      const emailSignUpInput = document.getElementsByName('email')[0];
-      const passwordSignUpInput = document.getElementsByName('password')[0];
-      const confirmPasswordSignUpInput = document.getElementsByName('repassword')[0];
+    //   const usernameSignUpInput = document.getElementsByName('login')[0];
+    //   const emailSignUpInput = document.getElementsByName('email')[0];
+    //   const passwordSignUpInput = document.getElementsByName('password')[0];
+    //   const confirmPasswordSignUpInput = document.getElementsByName('repassword')[0];
 
-      const userData = {
-        username: usernameSignUpInput.value.trim(),
-        email: emailSignUpInput.value.trim(),
-        password: passwordSignUpInput.value.trim(),
-        confirmPassword: confirmPasswordSignUpInput.value.trim(),
-      };
+    //   const userData = {
+    //     username: usernameSignUpInput.value.trim(),
+    //     email: emailSignUpInput.value.trim(),
+    //     password: passwordSignUpInput.value.trim(),
+    //     confirmPassword: confirmPasswordSignUpInput.value.trim(),
+    //   };
 
-      const validationResult = Validate.signUp(userData);
+    //   const validationResult = Validate.signUp(userData);
 
-      if (validationResult !== undefined) {
-        alert.innerText = validationResult;
-        alert.style.visibility = 'visible';
-        return;
-      }
+    //   if (validationResult !== undefined) {
+    //     alert.innerText = validationResult;
+    //     alert.style.visibility = 'visible';
+    //     return;
+    //   }
 
-      Request.signUp({
-        body: userData,
-        alertObject: alert,
-      });
-    });
+    //   Request.signUp({
+    //     body: userData,
+    //     alertObject: alert,
+    //   });
+    // });
   }
 }
