843587b454495f648bbe21c220a9b8c64255ab0a
lida24 <lidalenkova204@gmail.com>
2021-09-29 02:10:21 +0300

    remove unusing links

--- public/src/models/ProductModel.js
+++ public/src/models/ProductModel.js
@@ -85,43 +85,45 @@ export default class ProductModel {
           </div>
         </div>
       </div>
-
     </script>`;
 
-/*
+    var source   = $("#entry-template").html();
+    var template = Handlebars.compile(source);
 
-    this.#parent.innerHTML = `
-        <div class="content">
-      <div class="back-to-result">
-        <a href="/">Вернуться в каталог</a>
-      </div>
-      <div class="details">
-        <div class="details-image">
-          <img src="${product.image}" alt="${product.name}" />
-        </div>
-        <div class="details-info">
-          <ul>
-            <li>
-              <h1>${product.name}</h1>
-            </li>
-            <li>
-            ${Rating.render({
-      value: product.rating,
-    })}
-            </li>
-            <li>
-              Цена: <strong>$${product.price}</strong>
-            </li>
-            <li>
-              Описание:
-              <div>
-                Здесь будет прекрасное описание товара
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>`;*/
+    /*
+    
+        this.#parent.innerHTML = `
+            <div class="content">
+          <div class="back-to-result">
+            <a href="/">Вернуться в каталог</a>
+          </div>
+          <div class="details">
+            <div class="details-image">
+              <img src="${product.image}" alt="${product.name}" />
+            </div>
+            <div class="details-info">
+              <ul>
+                <li>
+                  <h1>${product.name}</h1>
+                </li>
+                <li>
+                ${Rating.render({
+          value: product.rating,
+        })}
+                </li>
+                <li>
+                  Цена: <strong>$${product.price}</strong>
+                </li>
+                <li>
+                  Описание:
+                  <div>
+                    Здесь будет прекрасное описание товара
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>`;*/
 
     const backBtn = document.getElementsByClassName('back-to-result')[0];
     backBtn.addEventListener('click', (e) => {
