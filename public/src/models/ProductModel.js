3c11fcb60d17f5e45ca85db4c0ce865b1e2560ca
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-09-29 03:46:05 +0300

    asdf

--- public/src/models/ProductModel.js
+++ public/src/models/ProductModel.js
@@ -90,7 +90,7 @@ export default class ProductModel {
     </script>
 `;
 
-
+    console.log('kek1');
     console.log($("#entry-template"));
     console.log('kek');
     this.#parent.innerHTML = $("#entry-template");
