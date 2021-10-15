8d000623c21d8bb03b6365e065fa1226050cf3a2
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-15 19:00:04 +0300

    отрефакторил ajax

--- public/callbacks/ajax.js
+++ public/callbacks/ajax.js
@@ -1,33 +1,8 @@
 /* eslint-disable import/extensions */
 import eventBus from '../events/eventBus.js';
 import ajax from '../scripts/ajax.js';
-import user from '../context/user.js';
 
 const backendAddress = 'https://ozonback.herokuapp.com';
-
-// export const signin = (data) => {
-//   console.log(data);
-//   ajax.post({
-//     url: `${backendAddress}/login`,
-//     body: data
-//   })
-//     .then(({ responseText }) => JSON.parse(responseText))
-//     .then((responseObj) => {
-//       user.set(responseObj);
-//       console.log(user);    //==========================
-
-//       eventBus.emit('showView', {
-//         name: 'Homepage'
-//       });
-//     })
-//     .catch(({ responseText }) => {
-//       const response = JSON.parse(responseText);
-//       eventBus.emit('signinDataError', response['error description']);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// };
 
 export const signin = (data) => {
   console.log(data);
@@ -42,7 +17,6 @@ export const signin = (data) => {
       eventBus.emit('signin fail', responseText);
     });
 };
-
 
 export const signup = (data) => {
   console.log(data);
