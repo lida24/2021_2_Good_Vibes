488b98eefc23bd099f7bf566140186c96c9d4d67
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-17 23:39:41 +0300

    asdfasdf

--- public/homepage/view.js
+++ public/homepage/view.js
@@ -6,8 +6,8 @@ import View from '../scripts/view.js';
 import homepageEvents from './events.js';
 import homepageListeners from './listeners.js';
 
-// const homepageUrl = './templates/homepage.handlebars';
-const homepageUrl = './template.handlebars';
+const homepageUrl = './templates/homepage.handlebars';
+// const homepageUrl = './template.handlebars';
 
 export default class Homepage extends View {
   #url = homepageUrl;
