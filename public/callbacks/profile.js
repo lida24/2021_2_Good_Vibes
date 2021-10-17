e76160df3d1376cc075b6187789efa0f105fc462
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-17 22:03:54 +0300

    asdfasdf

--- public/callbacks/profile.js
+++ public/callbacks/profile.js
@@ -1,9 +1,12 @@
 /* eslint-disable import/extensions */
 import eventBus from '../events/eventBus.js';
+import user from '../context/user.js';
 
 export const sendLogin = () => {
-    // const login = document.getElementsByName('login')[0].value.trim();
-    // this.changeLogin(login);
+    const login = document.getElementsByName('login')[0].value.trim();
+    this.changeLogin(login);
 
-    console.log('update click');
+    console.log(user);
 };
+
+export a = 0;
