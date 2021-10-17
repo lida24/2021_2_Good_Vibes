5d74ab70fc971a353009f716ea891fc9235a860d
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-17 23:30:14 +0300

    asdfasdf

--- public/context/hood.js
+++
@@ -1,3 +0,0 @@
-const HoodContext = {};
-
-export default HoodContext;
--- public/context/product.js
+++
@@ -1,9 +0,0 @@
-const productContext = {
-  id: 100500,
-  name: 'product name',
-  image: '../img/cloth1.png',
-  price: 250,
-  rating: 4
-};
-
-export default productContext;
