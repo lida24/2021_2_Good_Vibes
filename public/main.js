44c1c574fd56da56304ad9531cd30a07e039e807
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-18 17:15:58 +0300

    asdfasdf

--- public/main.js
+++ public/main.js
@@ -7,13 +7,12 @@ import Router from './scripts/router.js';
 
 // import route from './scripts/_router.js';
 
+const router = new Router(document.getElementsByClassName('grid-container')[0]);
+router.start();
+
+
 eventBus.on('init', init);
 eventBus.emit('init');
 eventBus.add(viewDispatcherListeners);
 eventBus.add(ajaxListeners);
 
-const router = new Router(document.getElementsByClassName('grid-container')[0]);
-
-// console.log(router);
-
-router.start();
