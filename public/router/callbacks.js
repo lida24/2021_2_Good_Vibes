a20aa8989bfd015427833f17acc3b597f79801d4
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-18 22:19:23 +0300

    asdfasdf

--- public/router/callbacks.js
+++ public/router/callbacks.js
@@ -1,16 +1,16 @@
 export const rout = (name) => {
   console.log('rout', name);
 
-  if (window.location.pathname !== path) {
-    const historyState = {
-      state: this.routes[path].state
-    };
+  // if (window.location.pathname !== path) {
+  //   const historyState = {
+  //     state: this.routes[path].state
+  //   };
 
-    window.history.pushState(
-      historyState,
-      this.routes[path].state
-    );
-  }
+  //   window.history.pushState(
+  //     historyState,
+  //     this.routes[path].state
+  //   );
+  // }
 
 };
 
