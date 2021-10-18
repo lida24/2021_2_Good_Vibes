a3094351cedc7282568c045c80b443639052ddd0
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-18 15:41:15 +0300

    asdfasdf

--- public/main.js
+++ public/main.js
@@ -4,7 +4,11 @@ import { init } from './viewDispatcher/callbacks.js';
 import viewDispatcherListeners from './viewDispatcher/listeners.js';
 import ajaxListeners from './ajax/listeners.js';
 
+import route from './scripts/router.js';
+
 eventBus.on('init', init);
 eventBus.emit('init');
 eventBus.add(viewDispatcherListeners);
 eventBus.add(ajaxListeners);
+
+route.start();
