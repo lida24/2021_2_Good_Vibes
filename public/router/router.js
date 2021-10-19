efa96af37e0b119ce17a1738d09d3e5ae63ba9b5
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-19 20:59:31 +0300

    навигация по продуктам работает

--- public/router/router.js
+++ public/router/router.js
@@ -99,7 +99,8 @@ class Router {
 
       const requieredState = this.urlHandler(currentPath);
 
-      eventBus.emit(`${requieredState} state request`, id);
+      // eventBus.emit(`${requieredState} state request`, id);
+      eventBus.emit(`${requieredState} state request`);
     });
 
     const currentPath = window.location.pathname;
