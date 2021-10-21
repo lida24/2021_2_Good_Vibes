806e4ec5d3e8b882e367b15e55cc3ba99c1b75d3
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-21 23:58:43 +0300

    теперь точно работает роутинг

--- public/router/callbacks.js
+++ public/router/callbacks.js
@@ -6,18 +6,24 @@ export const rout = (name) => {
 export const add = (name) => {
   console.log('hist add', name);
   console.log(window.location.pathname);
+  console.log(window.location.search);
 
-  if (window.location.pathname.slice(1) !== name) {
+  let path = window.location.pathname.slice(1);
+  const params = window.location.search;
+
+  if (params !== '') {
+    path += params;
+  }
+
+  if (path !== name) {
     const historyState = {
       state: name
     };
 
     window.history.pushState(
-      // window.history.replaceState(
       historyState,
       name,
       name
     );
   }
-  // console.log(window.history);
 };
