7889100daeaf7357ff6fc3c8afd32081511ce059
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-10-21 22:43:31 +0300

    работает роутинг (более менее)

--- public/viewDispatcher/history.js
+++ public/viewDispatcher/history.js
@@ -6,11 +6,8 @@ export const add = (path) => {
 };
 
 export const addProduct = (responseText) => {
-  // eventBus.emit('history add', `product/?id=${path.match(/.*?id=(\d+)*/)[1]}`);
-  // console.log(path.match(/.*?id=(\d+)*/));
   console.log(JSON.parse(responseText));
   const { id } = JSON.parse(responseText);
   eventBus.emit('history add', `product?id=${id}`);
-  // eventBus.emit('history add', 'product');
   console.log(id);
 };
