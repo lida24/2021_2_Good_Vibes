ab3eeedc415634d3e3e4944e88c69283d07bd5cb
lida24 <lidalenkova204@gmail.com>
2021-10-24 21:55:15 +0300

    hp api

--- public/cart/callbacks.js
+++ public/cart/callbacks.js
@@ -3,14 +3,3 @@ export const cartResponse = () => {
     name: 'Cart'
   });
 };
-
-export const getCartItems = () => {
-  const cartItems = localStorage.getItem('cartItems')
-    ? JSON.parse(localStorage.getItem('cartItems'))
-    : [];
-  return cartItems;
-};
-
-export const setCartItems = (cartItems) => {
-  localStorage.setItem('cartItems', JSON.stringify(cartItems));
-};
