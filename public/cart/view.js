ddb54df28d781e352ccbc80ce2981029f77a1071
lida24 <lidalenkova204@gmail.com>
2021-10-28 19:59:37 +0300

    api cart

--- public/cart/view.js
+++ public/cart/view.js
@@ -30,12 +30,54 @@ export default class Cart extends View {
     this.element.innerHTML = html;
   }
 
+  #createItemsHTML() {
+    const ratingParent = this.element.getElementsByClassName('items')[0];
+    const ratingElem = document.createElement('div');
+    ratingElem.className = 'items';
+
+    const cartItems = cart.getCartItems();
+
+    cartItems.length === 0
+              ? '<div>Cart is empty. <a href="/#/">Go Shopping</a>'
+              : cartItems
+                  .map(
+                    (item) => `
+            <li>
+              <div class="cart-image">
+                <img src="${item.image}" alt="${item.name}" />
+              </div>
+              <div class="cart-name">
+                <div>
+                  <a href="/#/product/${item.product}">
+                    ${item.name}
+                  </a>
+                </div>
+                <div>
+                  Qty: 
+                  <select class="qty-select" id="${item.product}">
+                    <option value="1">1</option>
+                  </select>
+                  <button type="button" class="delete-button" id="${item.product}">
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <div class="cart-price">
+                $${item.price}
+              </div>
+            </li>
+            `
+                  )
+                  .join('\n')
+  }
+
+
   async render() {
     await this.#renderHTML();
     eventBus.add(cartListeners);
     this.#generateEvents(this.element);
-    const cartItems = cart.getCartItems();
-    console.log(cartItems);
+    this.#createItemsHTML();
+    //console.log(cartItems);
     return this.show();
   }
 
