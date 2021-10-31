01ad1d3dc682053179d63fe7f5bf5c029a7d3e1d
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-11-01 02:50:08 +0300

    зафиксил баги с отрисовкой корзины. Вроде бы

--- public/router/listeners.js
+++ public/router/listeners.js
@@ -8,25 +8,25 @@ const routerListeners = [
       router.add
     ]
   },
+  // {
+  //   event: 'hood render finished',
+  //   callback: [
+  //     router.rout
+  //   ]
+  // },
+
   {
-    event: 'hood render finished',
+    event: 'cookie check success',
     callback: [
       router.rout
     ]
   },
-
-  // {
-  //   event: 'cookie check success',
-  //   callback: [
-  //     router.rout
-  //   ]
-  // },
-  // {
-  //   event: 'cookie check fail',
-  //   callback: [
-  //     router.rout
-  //   ]
-  // },
+  {
+    event: 'cookie check fail',
+    callback: [
+      router.rout
+    ]
+  },
 
   // {
   //   event: 'cart get success',
