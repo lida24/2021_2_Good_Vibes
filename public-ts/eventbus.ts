97d2842cef55067fb132d957e30eaaf6164bc73b
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-11-02 20:21:00 +0300

    dfasdfas

--- public-ts/eventbus.ts
+++ public-ts/eventbus.ts
@@ -1,76 +1,60 @@
+type Callback = (arg0: object) => void;
+
+type Connection = {
+  event: string,
+  callback: Callback
+};
+
 class EventBus {
-  private listeners: object;
+  private listeners: { event: Callback[] };
 
-  constructor() {
-    this.listeners = {};
-  }
+  public on(event: string, callback: Callback) {
+    this.listeners.event = this.listeners.event || [];
 
-  public on(event: string, callback: (arg0: object) => void) {
-    this.listeners[event] = this.listeners[event] || []
-
-    if (this.listeners[event].includes(callback)) {
+    if (this.listeners.event.includes(callback)) {
       return;
     }
 
-    this.listeners[event].push(callback);
+    this.listeners.event.push(callback);
+  }
+
+  public off(event: string, callback: Callback) {
+    this.listeners.event = this.listeners.event
+      .filter((listener) => listener !== callback);
+  }
+
+  public emit(event: string, data: object) {
+    if (!this.listeners.event) {
+      console.log(`${event} without listeners`);
+      return;
+    }
+
+    this.listeners.event.forEach((listener) => listener(data));
+  }
+
+  public add(array: Connection[]) {
+    array.forEach(({ event, callback }) => {
+      if (Array.isArray(callback)) {
+        callback.forEach((callbackItem) => {
+          this.on(event, callbackItem);
+        });
+        return;
+      }
+      this.on(event, callback);
+    });
+  }
+
+  public delete(array: Connection[]) {
+    array.forEach(({ event, callback }) => {
+      if (Array.isArray(callback)) {
+        callback.forEach((callbackItem) => {
+          this.off(event, callbackItem);
+        });
+        return;
+      }
+      this.off(event, callback);
+    });
   }
 }
 
 export default new EventBus();
-
-// class EventBus {
-//   #listeners;
-
-//   constructor() {
-//     this.#listeners = {};
-//   }
-
-//   on(event, callback) {
-//     this.#listeners[event] = this.#listeners[event] || [];
-
-//     if (this.#listeners[event].includes(callback)) {
-//       return;
-//     }
-
-//     this.#listeners[event].push(callback);
-//   }
-
-//   off(event, callback) {
-//     this.#listeners[event] = this.#listeners[event]
-//       .filter((listener) => listener !== callback);
-//   }
-
-//   emit(event, data) {
-//     if (!this.#listeners[event]) {
-//       console.log(`${event} without listeners`);
-//       return;
-//     }
-//     this.#listeners[event].forEach((listener) => listener(data));
-//   }
-
-//   add(array) {
-//     array.forEach(({ event, callback }) => {
-//       if (Array.isArray(callback)) {
-//         callback.forEach((callbackItem) => {
-//           this.on(event, callbackItem);
-//         });
-//         return;
-//       }
-//       this.on(event, callback);
-//     });
-//   }
-
-//   delete(array) {
-//     array.forEach(({ event, callback }) => {
-//       if (Array.isArray(callback)) {
-//         callback.forEach((callbackItem) => {
-//           this.off(event, callbackItem);
-//         });
-//         return;
-//       }
-//       this.off(event, callback);
-//     });
-//   }
-// }
-
-// export default new EventBus();
--- public-ts/main.ts
+++ public-ts/main.ts
@@ -1,23 +1,3 @@
-//Получая элемент, мы можем указать, какого он типа
-//В данном случае - поле ввода
-//Это говорит, что полученный элемент точно содержит свойства value и valueAsNumber, которые нам нужны
-//Если мы не укажем тип HTMLInputElement, компилятор не поймет, что у этого элемента есть свойство value, и откажется компилировать
-const num1 = <HTMLInputElement>document.getElementById("num1");
-const num2 = <HTMLInputElement>document.getElementById("num2");
+import bus from './eventbus';
 
-//Можно и не указывать тип элемента, если вам не нужны какие-то специфические свойства
-const result = document.getElementById("result");
-const btn = document.getElementById("btn");
-
-//Пока ещё не все современные браузеры поддерживают стрелочные функции, но компилятор TypeScript заменит её на обычную анонимную функцию
-//При условии, что в конфиге указана спецификация es5
-btn.addEventListener("click", (e) => { Sum(); });
-
-function Sum() {
-  //В эту переменную можно добавить только числовое значение
-  //Однако ввод всё равно стоит проверять, потому что пользователь может ввести что угодно вместо числа
-  let sum: number = num1.valueAsNumber + num2.valueAsNumber;
-
-  //Тип свойства innerHTML - string, поэтому мы должны конвертировать переменную sum в строку
-  result.innerHTML = sum.toString();
-}
+console.log(bus);
