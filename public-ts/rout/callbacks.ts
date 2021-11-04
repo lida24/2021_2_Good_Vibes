6f836b92cd35a78e17bd452424dd7d1cbae15d1b
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-11-05 00:30:45 +0300

    dfasdfas

--- public-ts/rout/callbacks.ts
+++ public-ts/rout/callbacks.ts
@@ -2,6 +2,15 @@ import { Callback } from '../types';
 import router from './router';
 
 export const init: Callback = () => {
+  const root = <HTMLElement>document.getElementsByClassName('grit-container')[0];
+  router.set(root);
+
+  router
+    .register('/', 'profile')
+    .register('/profile', 'profile')
+    .register('/signin', 'signIn')
+    .register('/signup', 'signUp');
+
   router.start();
 };
 
