4be3b0a1568596173fb3e3f8404daf934c7892f3
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-11-05 03:43:39 +0300

    dfasdfas

--- public-ts/dispatcher/denied/callbacks.ts
+++ public-ts/dispatcher/denied/callbacks.ts
@@ -3,12 +3,18 @@ import { Callback } from '../../types';
 
 export const signIn: Callback = () => {
   console.error('signIn state denied');
+
+  bus.emit('profile state request', undefined);
 };
 
 export const signUp: Callback = () => {
   console.error('signUp state denied');
+
+  bus.emit('profile state request', undefined);
 };
 
 export const profile: Callback = () => {
   console.error('profile state denied');
+
+  bus.emit('signIn state request', undefined);
 };
--- public-ts/dispatcher/request/callbacks.ts
+++ public-ts/dispatcher/request/callbacks.ts
@@ -1,14 +1,29 @@
 import bus from '../../init/bus';
+import user from '../../object/user/user';
 import { Callback } from '../../types';
 
 export const signIn: Callback = () => {
+  if (user.isAutorize) {
+    bus.emit('signIn state denied', undefined);
+    return;
+  }
+
   bus.emit('signIn state confirmed', { pathname: '/signin' });
 };
 
 export const signUp: Callback = () => {
+  if (user.isAutorize) {
+    bus.emit('signUp state denied', undefined);
+    return;
+  }
+
   bus.emit('signUp state confirmed', { pathname: '/signup' });
 };
 
 export const profile: Callback = () => {
+  if (!user.isAutorize) {
+    bus.emit('profile state denied', undefined);
+  }
+
   bus.emit('profile state confirmed', { pathname: '/profile' });
 };
