c9188bb26817275fe70fada132fdb0f692081707
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-11-10 16:53:57 +0300

    rk2

--- src/view/aside/callbacks.ts
+++ src/view/aside/callbacks.ts
@@ -11,44 +11,117 @@ export const hide: Callback = () => {
   asideObj.classList.remove('open');
 };
 
+// export const addCategory: (obj: Category, parent: HTMLElement) => HTMLElement = (obj, parent) => {
+//   const { name, description } = obj;
+
+//   const child = <HTMLLIElement>document.createElement('li');
+
+//   const href = <HTMLAnchorElement>document.createElement('a');
+//   href.href = '#';
+//   // href.innerHTML = name;
+//   href.className = 'categories__link';
+//   href.text = `${name} ${description}`;
+
+//   const span = <HTMLSpanElement>document.createElement('span');
+//   span.className = 'categories__span-fa';
+
+//   const i = <HTMLElement>document.createElement('i');
+//   i.className = 'fa fa-chevron-right';
+
+//   span.appendChild(i);
+
+//   href.appendChild(span);
+
+//   child.appendChild(href);
+
+//   parent.appendChild(child);
+
+//   href.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     // eventBus.emit('category request', name);
+//     bus.emit('category state request', { name });
+//   });
+
+//   return child;
+// };
+
+export const addLiEvents: (li: HTMLLIElement, obj: Category) => void = (li, obj) => {
+  const { name } = obj;
+
+  li.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const target = <HTMLElement>event.target;
+
+    if (target.tagName === 'A') {
+      bus.emit('category state request', { name });
+      return;
+    }
+
+    // if (target.tagName !== 'SPAN') return;
+
+    const ulCollection = li.parentElement.getElementsByTagName('ul');
+
+    // const ulCollection = li.parentNode.querySelector('ul');
+
+    if (!ulCollection) return;
+
+    // ulCollection.hidden = !ulCollection.hidden;
+
+    // if (ulCollection.hidden) {
+    //   target.classList.add('hide');
+    //   target.classList.remove('show');
+    // } else {
+    //   target.classList.add('show');
+    //   target.classList.remove('hide');
+    // }
+
+    Array.from(ulCollection).forEach((child) => {
+      // eslint-disable-next-line no-param-reassign
+
+      child.hidden = !child.hidden;
+
+      // child.hidden = true;
+    });
+
+    // const ul = li.parentNode.querySelector('ul');
+    // ul.hidden = false;
+    // let nextSubling = ul.nextSibling;
+    // while (nextSubling) {
+    //   nextSubling
+    // }
+  });
+};
+
 export const addCategory: (obj: Category, parent: HTMLElement) => HTMLElement = (obj, parent) => {
   const { name, description } = obj;
 
-  const child = <HTMLLIElement>document.createElement('li');
+  const child = <HTMLUListElement>document.createElement('ul');
 
-  const href = <HTMLAnchorElement>document.createElement('a');
-  href.href = '#';
-  // href.innerHTML = name;
-  href.className = 'categories__link';
-  href.text = `${name} ${description}`;
+  const li = <HTMLLIElement>document.createElement('li');
+  addLiEvents(li, obj);
+  child.appendChild(li);
 
-  const span = <HTMLSpanElement>document.createElement('span');
-  span.className = 'categories__span-fa';
+  const link = <HTMLSpanElement>document.createElement('a');
+  link.innerHTML = description;
+  link.className = 'categories__span-fa';
+  li.appendChild(link);
 
-  const i = <HTMLElement>document.createElement('i');
-  i.className = 'fa fa-chevron-right';
-
-  span.appendChild(i);
-
-  href.appendChild(span);
-
-  child.appendChild(href);
+  if (obj.children) {
+    const span = <HTMLAnchorElement>document.createElement('span');
+    li.appendChild(span);
+    // span.innerHTML = 'развернуть';
+    const i = <HTMLElement>document.createElement('i');
+    i.className = 'fa fa-chevron-right';
+    span.appendChild(i);
+  }
 
   parent.appendChild(child);
-
-  href.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    // eventBus.emit('category request', name);
-    bus.emit('category state request', { name });
-  });
-
   return child;
 };
 
 export const handleObj: (obj: Category, parent: HTMLElement) => void = (obj, parent) => {
-  // console.log(obj.name);
-
   const child = addCategory(obj, parent);
 
   if (obj.children) {
@@ -59,9 +132,9 @@ export const handleObj: (obj: Category, parent: HTMLElement) => void = (obj, par
 };
 
 export const parseCategoryObject: Callback = (obj: Category) => {
-  console.log('parseCategoryObject');
+  // console.log('parseCategoryObject');
 
-  console.log(obj);
+  // console.log(obj);
 
   const parent = <HTMLElement>document.getElementsByClassName('categories')[0];
 
@@ -81,7 +154,7 @@ export const parse: Callback = (response: AjaxResponse) => {
   const { responseText } = response;
   Promise.resolve()
     .then(() => JSON.parse(responseText))
-    .then((parsedObj) => parseCategoryObject(parsedObj))
+    .then((parsedObj: Category) => parseCategoryObject(parsedObj))
 
     .catch((err) => console.error('category response parse error', err));
 };
