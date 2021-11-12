df6bbae8269805b40c8ff4c54f2f111d41014b4b
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-11-12 03:48:28 +0300

    сделал кликабельной всю карточку товара, а не только имя и картинку

--- src/view/productCard/events.ts
+++ src/view/productCard/events.ts
@@ -1,20 +1,27 @@
 import bus from '../../init/bus';
 import { Product } from '../../types';
 
-const initEvents = (self: HTMLElement, context: Product) => {
+const initEvents: (self: HTMLElement, context: Product) => void = (self, context) => {
   const { id } = context;
 
-  // ------------------
-  const productImgHref = <HTMLAnchorElement>self.getElementsByClassName('product__link-img')[0];
-  productImgHref.addEventListener('click', (event) => {
-    event.preventDefault();
+  // // ------------------
+  // const productImgHref = <HTMLAnchorElement>self.getElementsByClassName('product__link-img')[0];
+  // productImgHref.addEventListener('click', (event) => {
+  //   event.preventDefault();
 
-    bus.emit('image click', { id });
-  });
+  //   bus.emit('image click', { id });
+  // });
+
+  // // ------------------
+  // const productNameHref = <HTMLAnchorElement>self.getElementsByClassName('product__link-name')[0];
+  // productNameHref.addEventListener('click', (event) => {
+  //   event.preventDefault();
+
+  //   bus.emit('product name click', { id });
+  // });
 
   // ------------------
-  const productNameHref = <HTMLAnchorElement>self.getElementsByClassName('product__link-name')[0];
-  productNameHref.addEventListener('click', (event) => {
+  self.addEventListener('click', (event) => {
     event.preventDefault();
 
     bus.emit('product name click', { id });
