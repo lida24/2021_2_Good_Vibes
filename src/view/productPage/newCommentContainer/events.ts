14b0fb9adfe02ef27cceabaf3c4a5afd02aaa898
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-11-21 23:50:48 +0300

    dfasdfasdf

--- src/view/productPage/newCommentContainer/events.ts
+++ src/view/productPage/newCommentContainer/events.ts
@@ -4,7 +4,13 @@ import { Product } from '../../../types';
 const initEvents: (self: HTMLElement) => void = (self) => {
   // ---------------------------
   const addCommentBtn = <HTMLButtonElement>self.getElementsByClassName('add-comment-btn')[0];
-  addCommentBtn.addEventListener('click', () => {
+  // addCommentBtn.addEventListener('click', () => {
+  //   bus.emit('add comment button click', undefined);
+  // });
+
+  self.addEventListener('submit', (event) => {
+    event.preventDefault();
+
     bus.emit('add comment button click', undefined);
   });
 };
