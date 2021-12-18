2eef5b0d94d65d14a2929b6147ca3703391f664d
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-12-18 23:01:25 +0300

    запретил ввод в поля фильтра чего либо, кроме цифр

--- src/components/searchFiltersContainer/callbacks.ts
+++ src/components/searchFiltersContainer/callbacks.ts
@@ -3,26 +3,32 @@ import searchParams from "../../services/search/params";
 import { Callback, Product } from "../../types";
 
 export const getParams: Callback = (array: Product[]) => {
-  
+
   const minPriceInput = <HTMLInputElement>document.getElementsByClassName('search-filter-amount__from')[0];
   const maxPriceInput = <HTMLInputElement>document.getElementsByClassName('search-filter-amount__to')[0];
   const minRatingInput = <HTMLInputElement>document.getElementsByClassName('search-filter-rating__from')[0];
   const maxRatingInput = <HTMLInputElement>document.getElementsByClassName('search-filter-rating__to')[0];
 
 
-  if (+minPriceInput.value <= searchParams.minPriceStatic 
-    && minPriceInput.value != "") {
-    minPriceInput.value = searchParams.minPriceStatic.toString();
-  } else {
-    minPriceInput.value = searchParams.minPrice.toString();
-  }
+  // if (+minPriceInput.value <= searchParams.minPriceStatic
+  //   && minPriceInput.value != "") {
+  //   minPriceInput.value = searchParams.minPriceStatic.toString();
+  // } else {
+  //   minPriceInput.value = searchParams.minPrice.toString();
+  // }
 
-  if (+maxPriceInput.value >= searchParams.maxPriceStatic
-    && minPriceInput.value != "") {
-    maxPriceInput.value = searchParams.maxPriceStatic.toString();
-  } else {
-    maxPriceInput.value = searchParams.maxPrice.toString();
-  }
+  // if (+maxPriceInput.value >= searchParams.maxPriceStatic
+  //   && minPriceInput.value != "") {
+  //   maxPriceInput.value = searchParams.maxPriceStatic.toString();
+  // } else {
+  //   maxPriceInput.value = searchParams.maxPrice.toString();
+  // }
+  // minRatingInput.value = searchParams.minRating.toString();
+  // maxRatingInput.value = searchParams.maxRating.toString();
+
+  minPriceInput.value = searchParams.minPrice.toString();
+  maxPriceInput.value = searchParams.maxPrice.toString();
+
   minRatingInput.value = searchParams.minRating.toString();
   maxRatingInput.value = searchParams.maxRating.toString();
 };
@@ -36,19 +42,26 @@ export const setFiltersParams: Callback = () => {
   const maxRatingInput = <HTMLInputElement>document.getElementsByClassName('search-filter-rating__to')[0];
 
 
-  if (+minPriceInput.value < searchParams.minPriceStatic) {
-    searchParams.minPrice = searchParams.minPriceStatic
-    minPriceInput.value = searchParams.minPriceStatic.toString();
-  } else {
-    minPriceInput.value = searchParams.minPrice.toString();
-  }
+  // if (+minPriceInput.value < searchParams.minPriceStatic) {
+  //   searchParams.minPrice = searchParams.minPriceStatic
+  //   minPriceInput.value = searchParams.minPriceStatic.toString();
+  // } else {
+  //   minPriceInput.value = searchParams.minPrice.toString();
+  // }
 
-  if (+maxPriceInput.value > searchParams.maxPriceStatic) {
-    searchParams.maxPrice = searchParams.maxPriceStatic
-    maxPriceInput.value = searchParams.maxPriceStatic.toString();
-  } else {
-    maxPriceInput.value = searchParams.maxPrice.toString();
-  }
+  // if (+maxPriceInput.value > searchParams.maxPriceStatic) {
+  //   searchParams.maxPrice = searchParams.maxPriceStatic
+  //   maxPriceInput.value = searchParams.maxPriceStatic.toString();
+  // } else {
+  //   maxPriceInput.value = searchParams.maxPrice.toString();
+  // }
+  // minRatingInput.value = searchParams.minRating.toString();
+  // maxRatingInput.value = searchParams.maxRating.toString();
+
+  minPriceInput.value = searchParams.minPrice.toString();
+  maxPriceInput.value = searchParams.maxPrice.toString();
+
   minRatingInput.value = searchParams.minRating.toString();
   maxRatingInput.value = searchParams.maxRating.toString();
+
 };
