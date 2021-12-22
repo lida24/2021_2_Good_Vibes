9a0f30c89d7e56c98435bf0493712b098e7fef1f
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-12-22 03:00:04 +0300

    bug fixes

--- src/components/hood/view.ts
+++ src/components/hood/view.ts
@@ -8,6 +8,8 @@ import './hood.scss';
 
 import SearchInput from './searchInput/view';
 import user from '../../services/user/user';
+import PopupSearch from './popupSearch/view';
+import Navbar from './navbar-mobile/view';
 
 export default class Hood extends View implements ViewInterface {
   /* private async renderHTML() {
@@ -20,10 +22,30 @@ export default class Hood extends View implements ViewInterface {
     const html = compiledTemplate(this.context);
     this.self.innerHTML = html;
     initEvents(this.self);
-  } 
+  }
 
   public async render(): Promise<void> {
     await this.renderHTML();
+
+    // const searchInput = new SearchInput(undefined);
+
+    // this.self.getElementsByClassName('header__inner')[0].appendChild(searchInput.self);
+    // const search = <HTMLElement>this.self.getElementsByClassName('search-suggests')[0];
+    // search.style.visibility = 'visible';
+
+    // const popupSearch = new PopupSearch(undefined);
+
+    // this.self.getElementsByClassName('body')[0].appendChild(popupSearch.self);
+    // const popup = <HTMLElement>this.self.getElementsByClassName('search-suggests-popup')[0];
+    // popup.style.visibility = 'visible';
+
+    // const navbar = new Navbar(undefined);
+
+    // this.self.getElementsByClassName('body')[0].appendChild(navbar.self);
+    // const navbarMobile = <HTMLElement>this.self.getElementsByClassName('navbar-mobile')[0];
+    // navbarMobile.style.visibility = 'visible';
+    // searchInput.show();
+
     return this.show();
   }
 
