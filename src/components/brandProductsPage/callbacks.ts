f9368408fd56259969fc8fb6c92f7ad42de36fa7
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-12-25 01:17:30 +0300

    brand product fixed

--- src/components/brandProductsPage/callbacks.ts
+++ src/components/brandProductsPage/callbacks.ts
@@ -2,13 +2,7 @@ import { Callback, Product } from '../../types';
 import ProductCatdList from '../productCard/list';
 
 export const addProductArrayBrand: Callback = (array: Product[]) => {
-
-  debugger;
-
-  // const productContainer = document.getElementById('table-brand');
-
-  const productContainer = document.getElementsByClassName('product-table-inner')[0];
-
+  const productContainer = document.getElementById('product-table-body');
   productContainer.textContent = '';
 
   /*array.sort((a, b) => a.id - b.id); */
