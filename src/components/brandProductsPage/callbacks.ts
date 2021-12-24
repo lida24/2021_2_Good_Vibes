62506f478325c58e158b5f6573a62109eb13728e
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-12-25 02:51:40 +0300

    brand page bug fixed

--- src/components/brandProductsPage/callbacks.ts
+++ src/components/brandProductsPage/callbacks.ts
@@ -1,12 +1,15 @@
 import { Callback, Product } from '../../types';
 import ProductCatdList from '../productCard/list';
 
-export const addProductArrayBrand: Callback = (array: Product[]) => {
+
+export const addProductArrayBrand: Callback = (obj: { 'Products': Product[], 'BrandName': string }) => {
   const productContainer = document.getElementById('product-table-body');
   productContainer.textContent = '';
 
-  /*array.sort((a, b) => a.id - b.id); */
-  const viewArray = ProductCatdList.viewArray(array);
+  const title = <HTMLSpanElement>document.getElementsByClassName('product-table__title')[0];
+  title.textContent = obj.BrandName;
+
+  const viewArray = ProductCatdList.viewArray(obj.Products);
   viewArray?.forEach((cardView) => {
     productContainer.appendChild(cardView?.self);
   });
